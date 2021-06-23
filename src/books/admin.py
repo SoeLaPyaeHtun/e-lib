bd7a94b8236b33f2e1540ee0257abd027c4ff1d9
SoeLaPyaeHtun <soelapyaehtun@gmail.com>
2021-06-23 22:19:06 +0630

    add publishing models

--- src/books/admin.py
+++ src/books/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Author, Category, Book
+from .models import Author, Category, Book, Publishing
 # Register your models here.
 
 
 admin.site.register(Author)
+admin.site.register(Publishing)
 admin.site.register(Category)
 admin.site.register(Book)
--- src/books/models.py
+++ src/books/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 
 class Publishing(models.Model):
-    title = models.CharField(max_length=64)
+    title = models.CharField(max_length=64, blank=True, null=True)
+    headquarter = models.CharField(max_length=128, blank=True, null=True)
 
     def __str__(self):
         return self.title
@@ -32,7 +33,7 @@ class Category(models.Model):
 class Book(models.Model):
     name = models.CharField(max_length=64, blank=True, null=True)
     publishing = models.ForeignKey(
-        Publishing, on_delete=models.CASCADE, related_name="publishing")
+        Publishing, on_delete=models.CASCADE, related_name="publishing", blank=True, null=True)
     author = models.ForeignKey(
         Author, on_delete=models.CASCADE, related_name="author")
     category = models.ForeignKey(
